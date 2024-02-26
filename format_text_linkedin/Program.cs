7272728e3b3c12f78c0c524e826c971651170e1b
Damir Ramazanov <chikibambonika007@gmail.com>
2024-02-26 17:03:46 +0600

    format-size-text-output-file

--- format_text_linkedin/Program.cs
+++ format_text_linkedin/Program.cs
@@ -1,7 +1,18 @@
-﻿class Program
+﻿using format_text_linkedin.Requests;
+using format_text_linkedin.WorkTextFile;
+
+class Program
 {
     public static void Main(string[] args)
     {
+        PathFile pathFile = new PathFile();
+        string path = pathFile.GetFilePath();
+        string outputPath = @"C:\Users\AdreN\OneDrive\Рабочий стол\output.txt";
 
+        FormatLine formatLine = new FormatLine(path, outputPath);
+        formatLine.FormattingLinesInTextFile();
+
+        SizeText sizeText = new SizeText(outputPath);
+        sizeText.DeleteWords();
     }
 }
---
+++ format_text_linkedin/WorkTextFile/SizeText.cs
@@ -0,0 +1,46 @@
+﻿using format_text_linkedin.FormatText;
+using format_text_linkedin.LengthText;
+
+namespace format_text_linkedin.WorkTextFile
+{
+    internal class SizeText
+    {
+        private string _outputPath;
+        private int _lengthText;
+        private Length _length;
+        private UnnecessaryWords _unnecessaryWords;
+
+        public SizeText(string outputPath)
+        {
+            _outputPath = outputPath;
+
+            _length = new Length();
+            _unnecessaryWords = new UnnecessaryWords();
+        }
+
+        public void DeleteWords()
+        {
+            _lengthText = _length.GetLengthText(_outputPath);
+
+            if (_lengthText > 4000)
+            {
+                Console.WriteLine($"\nThe number of characters is more than 4000!!!\n" +
+                    $"Number of characters: {_lengthText}");
+
+                Console.Write($"\nDo you want to shorten the text to 4000 (y/n):");
+                string choiceUser = Console.ReadLine();
+
+                if (choiceUser.ToLower() == "y")
+                {
+                    _unnecessaryWords.DeleteUnnecessaryWords(_outputPath);
+                    _lengthText = _length.GetLengthText(_outputPath);
+                    Console.WriteLine($"Number of characters: {_lengthText}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Number of characters: {_lengthText}");
+            }
+        }
+    }
+}
