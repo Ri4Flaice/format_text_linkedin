5a81b207b5e76e0845b59a36c3622ca4189e42f9
Damir Ramazanov <chikibambonika007@gmail.com>
2024-02-26 16:41:23 +0600

    delete-unncessary-words-more-four-thousand

---
+++ format_text_linkedin/FormatText/UnnecessaryWords.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace format_text_linkedin.FormatText
+{
+    internal class UnnecessaryWords
+    {
+        public void DeleteUnnecessaryWords(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string text = string.Join(" ", lines);
+
+            string[] sentences = text.Split(new[] { ". " }, StringSplitOptions.None);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var sentence in sentences)
+            {
+                if (sb.Length + sentence.Length > 4000)
+                    break;
+
+                sb.Append(sentence);
+                sb.Append(". ");
+            }
+
+            File.WriteAllText(path, sb.ToString());
+        }
+    }
+}
---
+++ format_text_linkedin/LengthText/Length.cs
@@ -0,0 +1,13 @@
+﻿namespace format_text_linkedin.LengthText
+{
+    internal class Length
+    {
+        public int GetLengthText(string path)
+        {
+            string text = File.ReadAllText(path);
+            int length = text.Length;
+
+            return length;
+        }
+    }
+}
